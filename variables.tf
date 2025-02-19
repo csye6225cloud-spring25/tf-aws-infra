2069c10bd15c76c87cc492387be1b5ade746709f
Rahul Ramesh Patil <patil.rahu@northeastern.edu>
2025-02-19 10:48:07 -0500

    Add Terraform configuration files for VPC, subnets, NAT gateway, and routing

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "aws_region" {
+  type = string
+  description = "AWS region for the deployment"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "demo_vpc"
+  description = "Name of the VPC"
+}
+
+variable "vpc_cidr" {
+  type = string
+  description = "CIDR block for the VPC"
+}
+
+variable "availability_zones" {
+  type = list(string)
+  description = "List of availability zones to use"
+}
+
+variable "private_subnets" {
+  type = map(string)
+  description = "Map of private subnet names to their CIDR blocks"
+}
+
+variable "public_subnets" {
+  type = map(string)
+  description = "Map of public subnet names to their CIDR blocks"
+}
---
+++ vpc.tf
@@ -0,0 +1,8 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name        = var.vpc_name
+    Environment = "demo_environment"
+    Terraform   = "true"
+  }
+}
