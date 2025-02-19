500d01bc3b8c451432f6db61598846055ba9a978
Rahul Ramesh Patil <patil.rahu@northeastern.edu>
2025-02-19 09:47:37 -0500

    Added terraform script for - main,nat,outputs,provier and route.tf files

---
+++ route_associations.tf
@@ -0,0 +1,11 @@
+resource "aws_route_table_association" "public" {
+  route_table_id = aws_route_table.public_route_table.id
+  for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+}
+
+resource "aws_route_table_association" "private" {
+  route_table_id = aws_route_table.private_route_table.id
+  for_each       = aws_subnet.private_subnets
+  subnet_id      = each.value.id
+}
