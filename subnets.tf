c28bc293f98aa3e1a781165ff474b8ddf8b28883
Rahul Ramesh Patil <patil.rahu@northeastern.edu>
2025-02-25 21:01:21 -0500

    assf

--- subnets.tf
+++ subnets.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "public_subnets" {
   for_each                = var.public_subnets
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = each.value
-  availability_zone       = var.availability_zone1[index(keys(var.public_subnets), each.key) % length(var.availability_zones)]
+  availability_zone       = var.availability_zones[index(keys(var.public_subnets), each.key) % length(var.availability_zones)]
   map_public_ip_on_launch = true
   tags = {
     Name      = each.key
