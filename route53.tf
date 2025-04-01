683819a95ca869fff4f49eb657ce80c2e9143f1e
rahul912313 <patil.rahu@northeastern.edu>
2025-04-01 00:33:01 -0400

    route53

--- route53.tf
+++ route53.tf
@@ -2,7 +2,7 @@
 
 # Reference the existing hosted zone for dev.onerahul.me
 data "aws_route53_zone" "dev_zone" {
-  zone_id = "Z0624390ZXDW2D39OXCG"  
+  zone_id = "Z0624390ZXDW2D39OXCG"
 }
 
 # Create an A record alias pointing to the ALB
@@ -11,8 +11,8 @@ resource "aws_route53_record" "dev_alb" {
   name    = "dev.onerahul.me"
   type    = "A"
   alias {
-    name                   = aws_lb.app_alb.dns_name   # Reference to your ALB’s DNS name
-    zone_id                = aws_lb.app_alb.zone_id    # Reference to your ALB’s zone ID
+    name                   = aws_lb.app_alb.dns_name # Reference to your ALB’s DNS name
+    zone_id                = aws_lb.app_alb.zone_id  # Reference to your ALB’s zone ID
     evaluate_target_health = true
   }
 }
